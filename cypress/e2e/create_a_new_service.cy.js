936528818098de37dcb6eb500baca12a3ea1de62
Kevin Lee <kevinlee52@163.com>
2024-07-29 21:01:55 +0800

    add tests

--- cypress/e2e/create_a_new_service.cy.js
+++ cypress/e2e/create_a_new_service.cy.js
@@ -1,5 +1,27 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("create a new service", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:8002/");
   });
+
+  it("information", () => {
+    cy.get(".title").contains("Workspaces");
+  });
+
+  it("test 2nd", () => {
+    cy.get(".title").contains("Workspaces");
+  });
+
+  it("test 3rd", () => {
+    cy.get(".title").contains("Workspaces");
+  });
+
+  // it.only("the features on the homepage are correct", () => {
+  //   cy.get("dt")
+  //     .eq(0)
+  //     .contains(/4 courses/i)
+  //   cy.get("dt").eq(1).contains("25+ Lessons")
+  //   cy.get("dt")
+  //     .eq(2)
+  //     .contains(/free and open/i)
+  // })
 });
