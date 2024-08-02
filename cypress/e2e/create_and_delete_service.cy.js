513167a25fd5a87e9cefe5879d34b5cf7f97b267
Kevin Lee <kevinlee52@163.com>
2024-08-02 18:33:00 +0800

    encaps

--- cypress/e2e/create_and_delete_service.cy.js
+++ cypress/e2e/create_and_delete_service.cy.js
@@ -1,72 +1,35 @@
 describe("gateway service actions", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:8002/");
+    cy.navToDefaultWorkspace();
   });
 
   it("create a mock service", () => {
-    cy.get(".workspace-name").contains("default").click();
-    cy.get(".sidebar-menu-toggle").click();
-    cy.get('[data-testid="sidebar-item-gateway-services"]').click();
-    cy.get('[data-testid="new-gateway-service"]').click();
-    // cy.get('[data-testid="toolbar-add-gateway-service"]').click();
-    cy.get('[data-testid="gateway-service-name-input"]').type("mock_service");
-    cy.get('[data-testid="gateway-service-url-input"]').type(
-      "http://httpbin.org"
-    );
-    cy.get('[data-testid="form-submit"]').click();
-    cy.get(".sidebar-menu-toggle").click();
-    cy.get('[data-testid="sidebar-item-gateway-services"]').click();
-    cy.get('tbody > [data-testid="mock_service"]').should(($ms) => {
-      expect($ms.first()).to.contain("mock_service");
-    });
+    cy.navToServicesPage();
+    cy.addNewService("mock_service", "http://httpbin.org");
+    cy.navToServicesPage();
+    cy.validateServiceCreation("mock_service");
   });
 
   it("create a route for the mock service", () => {
-    cy.get(".workspace-name").contains("default").click();
-    cy.get(".sidebar-menu-toggle").click();
-    cy.get('[data-testid="sidebar-item-gateway-services"]').click();
-    cy.get('tbody > [data-testid="mock_service"]').click();
-    cy.get(".alert-message > .k-button").click();
-    cy.get('[data-testid="route-form-name"]').type("mock_route");
-    cy.get('[data-testid="route-form-paths-input-1"]').type("/mock_route");
-    cy.get('[data-testid="form-submit"]').click();
-    cy.get(".sidebar-menu-toggle").click();
-    cy.get('[data-testid="sidebar-item-routes"]').click();
-    cy.get('tbody > [data-testid="mock_route"]').should(($mr) => {
-      expect($mr.first()).to.contain("mock_route");
-    });
+    cy.navToServicesPage();
+    cy.addRouteToService("mock_service", "mock_route", "/mock_route");
+    cy.navToRoutePage();
+    cy.validateRouteCreation("mock_route");
   });
 
   it("delete the mock route", () => {
-    cy.get(".workspace-name").contains("default").click();
-    cy.get(".sidebar-menu-toggle").click();
-    cy.get('[data-testid="sidebar-item-routes"]').click();
-    cy.get('tr[data-testid="mock_route"]').click();
-    cy.get('[data-testid="header-actions"]').click();
-    cy.get(
-      '.danger > [data-testid="dropdown-item-trigger"] > .dropdown-item-trigger-label'
-    ).click();
-    cy.get('[data-testid="confirmation-input"]').type("mock_route");
-    cy.get('[data-testid="modal-action-button"]').click();
-    cy.get(".sidebar-menu-toggle").click();
-    cy.get('[data-testid="sidebar-item-routes"]').click();
+    cy.navToRoutePage();
+    cy.deleteRoute("mock_route");
+    cy.navToRoutePage();
     cy.get(".kong-ui-entities-routes-list").should(($mr) => {
       expect($mr.first()).to.not.contain("mock_route");
     });
   });
 
   it("delete the mock service", () => {
-    cy.get(".workspace-name").contains("default").click();
-    cy.get(".sidebar-menu-toggle").click();
-    cy.get('[data-testid="sidebar-item-gateway-services"]').click();
-    cy.get('div[data-testid="mock_service"]').click();
-    cy.get(
-      'div[data-testid="mock_service"] > .k-dropdown > [data-testid="k-dropdown-popover"]   .k-popover-content > ul > li[data-testid="action-entity-delete"]'
-    ).click();
-    cy.get('[data-testid="confirmation-input"]').type("mock_service");
-    cy.get('[data-testid="modal-action-button"]').click();
-    cy.get(".sidebar-menu-toggle").click();
-    cy.get('[data-testid="sidebar-item-gateway-services"]').click();
+    cy.navToServicesPage();
+    cy.deleteService("mock_service");
+    cy.navToServicesPage();
     cy.get(".kong-ui-entities-gateway-services-list").should(($ms) => {
       expect($ms.first()).to.not.contain("mock_service");
     });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,68 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("navToDefaultWorkspace", () => {
+  cy.visit("http://localhost:8002/");
+  cy.get(".workspace-name").contains("default").click();
+});
+
+Cypress.Commands.add("navToServicesPage", () => {
+  cy.get(".sidebar-menu-toggle").click();
+  cy.get('[data-testid="sidebar-item-gateway-services"]').click();
+});
+
+Cypress.Commands.add("navToRoutePage", () => {
+  cy.get(".sidebar-menu-toggle").click();
+  cy.get('[data-testid="sidebar-item-routes"]').click();
+});
+
+Cypress.Commands.add("addNewService", (name, url) => {
+  cy.get('[data-testid="new-gateway-service"]').click();
+  // cy.get('[data-testid="toolbar-add-gateway-service"]').click();
+  cy.get('[data-testid="gateway-service-name-input"]').type(name);
+  cy.get('[data-testid="gateway-service-url-input"]').type(url);
+  cy.get('[data-testid="form-submit"]').click();
+});
+
+Cypress.Commands.add("validateServiceCreation", (serviceName) => {
+  cy.get(`tbody > [data-testid=${serviceName}]`).should(($ms) => {
+    expect($ms.first()).to.contain(serviceName);
+  });
+});
+
+Cypress.Commands.add(
+  "addRouteToService",
+  (serviceName, routeName, routePath) => {
+    cy.get(`tbody > [data-testid=${serviceName}]`).click();
+    cy.get(".alert-message > .k-button").click();
+    cy.get('[data-testid="route-form-name"]').type(routeName);
+    cy.get('[data-testid="route-form-paths-input-1"]').type(routePath);
+    cy.get('[data-testid="form-submit"]').click();
+  }
+);
+
+Cypress.Commands.add("validateRouteCreation", (routeName) => {
+  cy.get(`tbody > [data-testid=${routeName}]`).should(($mr) => {
+    expect($mr.first()).to.contain(routeName);
+  });
+});
+
+Cypress.Commands.add("deleteRoute", (routeName) => {
+  cy.get(`tr[data-testid=${routeName}]`).click();
+  cy.get('[data-testid="header-actions"]').click();
+  cy.get(
+    '.danger > [data-testid="dropdown-item-trigger"] > .dropdown-item-trigger-label'
+  ).click();
+  cy.get('[data-testid="confirmation-input"]').type(routeName);
+  cy.get('[data-testid="modal-action-button"]').click();
+});
+
+Cypress.Commands.add("deleteService", (serviceName) => {
+  cy.get(`div[data-testid=${serviceName}]`).click();
+  cy.get(
+    'div[data-testid="mock_service"] > .k-dropdown > [data-testid="k-dropdown-popover"]   .k-popover-content > ul > li[data-testid="action-entity-delete"]'
+  ).click();
+  cy.get('[data-testid="confirmation-input"]').type(serviceName);
+  cy.get('[data-testid="modal-action-button"]').click();
+});
